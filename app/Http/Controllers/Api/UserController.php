8fa6f7a9fb5777a9c9cbfead846c44b38268b0b9
luis <proffine1@gmail.com>
2020-11-02 16:18:05 -0600

    changes

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -13,8 +13,6 @@ class UserController extends Controller
     private $private_key = "44745559505951506b633750";
 
     public function getUser(Request $request){
-        return response()->json(["error" => "can´t resolve host request"], 200);
-        /*
         $user_token = $request->user_token;
         $sagmCredentials = SagmCredential::all()->where('user_token', $user_token)->first();
 
@@ -26,7 +24,5 @@ class UserController extends Controller
         }
 
         return response()->json(["error" => "can´t resolve host request"], 400);
-        */
-        
     }
 }
